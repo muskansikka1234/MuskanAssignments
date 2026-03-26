074d80b708862f2dd3accf4d4fcb02f594fad07e
Muskan <muskansikka1234@gmail.com>
2026-03-26 14:55:16 +0530

    Added

---
+++ March26_Assignment/WebApiInAsp.netcoreMvcDemo/WebApiInAsp.netcoreMvcDemo/Migrations/20260324055244_add.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApiInAsp.netcoreMvcDemo.Migrations
+{
+    /// <inheritdoc />
+    public partial class add : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
