a1d19982c6b424ef06bac03105c4e8ddc6aed94b
Muskan <muskansikka1234@gmail.com>
2026-04-09 15:06:06 +0530

    Added

---
+++ Week5 Assignments/April9_Assignment/DeploymentDemo/DeploymentDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeploymentDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
